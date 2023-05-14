5fd757e20dba3ef76f5a4443accadc9f53d9d473
CodeR <118649094+brandbold-Ch@users.noreply.github.com>
2023-05-14 00:15:58 -0600

    Update

--- ClothesMarket/main.py
+++ ClothesMarket/main.py
@@ -76,7 +76,7 @@ async def login(response: Response, username: str = None, password: str = None) 
 def authorization(request: Request):
     load_dotenv()
     print("tk ", os.getenv("TOKEN_USER"))
-    print(request.headers.get("Authenticate"))
+    return True
 
 
 # -------------------------------Método para obtener datos de la DB-----------------
